174bec6ab2902822b26d4f7e6c59ae1f4c2e6b3d
Lê Thị Thắm <thamlt.23itb.vku.udn.vn>
2025-04-07 15:59:20 +0700

    Ex3_find_lonely_number

---
+++ find_lonely_number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findLonely(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        freq = {}  # Đếm số lần xuất hiện của mỗi số
+
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+
+        result = []
+        for num in nums:
+            if freq[num] == 1 and (num - 1 not in freq) and (num + 1 not in freq):
+                result.append(num)
+
+        return result
+
+
+sol = Solution()
+print(sol.findLonely([10, 6, 5, 8]))  # Output: [10, 8]
+print(sol.findLonely([1, 3, 5, 3]))  # Output: [1, 5]
