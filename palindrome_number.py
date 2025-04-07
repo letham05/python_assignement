d2edd846aa015b0a4d0a82f30b90de899f45336c
Lê Thị Thắm <thamlt.23itb.vku.udn.vn>
2025-04-07 15:40:37 +0700

    Ex2_palindrome_number

---
+++ palindrome_number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+        return x == reversed_half or x == reversed_half // 10
+
+
+# Test nhanh
+print(Solution().isPalindrome(121))  # True
+print(Solution().isPalindrome(-121))  # False
+print(Solution().isPalindrome(10))  # False
+print(Solution().isPalindrome(12321))  # True
+print(Solution().isPalindrome(0))  # True
